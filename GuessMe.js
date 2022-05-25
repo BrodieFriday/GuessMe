8fb531ddd7433fc65a033bb1a83e6e067f0af2f0
Brodie Friday <brodiefriday@Brodies-MacBook-Pro.local>
2022-05-25 10:38:44 -0500

    Added the caribou JS code and updated JS code in  mysteryAnimal3.html

--- GuessMe.js
+++ GuessMe.js
@@ -27,6 +27,8 @@ function giraffe(){
 function polarbear(){
 	let input = document.getElementById("ans").value;
 	if(input==="Polar bear"|| input==="polar bear" || input==="polarbear" || input ==="Polarbear"){
+		var colorGreen = document.getElementById("c");
+		colorGreen.style.color="lawngreen";
 		document.getElementById("c").innerHTML = "That is Correct!!"
 		document.getElementById("h").innerHTML = "Scroll down to read more"
 		document.getElementById("header").innerHTML = "Info:"
@@ -34,5 +36,28 @@ function polarbear(){
 		document.getElementById("f").innerHTML = "&nbsp"
 		document.getElementById("full").style.display="block";
 	} 
+	else{
+		var colorRed=document.getElementById("c");
+		colorRed.style.color="#FF0000";
+		document.getElementById("c").innerHTML = "Try again"
+	}
 }
-			
+
+function caribou(){
+	let input = document.getElementById("ans").value;
+	if(input==="Caribou"|| input==="caribou"){
+		var colorGreen = document.getElementById("c");
+		colorGreen.style.color="lawngreen";
+		document.getElementById("c").innerHTML = "That is Correct!!"
+		document.getElementById("h").innerHTML = "Scroll down to read more"
+		document.getElementById("header").innerHTML = "Info:"
+		document.getElementById("i").innerHTML = "- This animal can be found in the tundra <br/> - Caribou shed their antler once a year."
+		document.getElementById("f").innerHTML = "&nbsp"
+		document.getElementById("full").style.display="block";
+	} 
+	else{
+		var colorRed=document.getElementById("c");
+		colorRed.style.color="#FF0000";
+		document.getElementById("c").innerHTML = "Try again"
+	}
+}
