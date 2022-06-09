13c07069df3d79a351b35882cb1d9304302762f2
Brodie Friday <brodieleefriday@gmail.com>
2022-06-09 10:28:08 -0500

    Added MA5 html file and changed some css color properties

--- GuessMe.js
+++ GuessMe.js
@@ -1,4 +1,4 @@
-function giraffeHint(){
+function foxHint(){
 	document.getElementById("hint").innerHTML = "Hint: This animal is very tall and lives where it's hot."
 }
 function polarbearHint(){
@@ -10,13 +10,16 @@ function caribouHint(){
 function bobcatHint(){
 	document.getElementById("hint").innerHTML = "HINT: This animal has the ability to climb trees."
 }
+function duckHint(){
+	document.getElementById("hint").innerHTML = "HINT: This animal can be found on my farms across the United States."
+}
 
 	
-function giraffe(){
+function fox(){
 	let input = document.getElementById("ans").value;
-	if(input==="Giraffe"|| input==="giraffe"){
+	if(input==="Fox"|| input==="fox"){
 		var colorGreen = document.getElementById("feedback");
-		colorGreen.style.color="lawngreen";
+		colorGreen.style.color="limegreen";
 		document.getElementById("feedback").innerHTML = "That is Correct!!"
 		document.getElementById("hint").innerHTML = "Scroll down to read more"
 		document.getElementById("nextButton").vlaue="Next"
@@ -35,7 +38,7 @@ function polarbear(){
 	let input = document.getElementById("ans").value;
 	if(input==="Polar bear"|| input==="polar bear" || input==="polarbear" || input ==="Polarbear"){
 		var colorGreen = document.getElementById("feedback");
-		colorGreen.style.color="lawngreen";
+		colorGreen.style.color="limegreen";
 		document.getElementById("feedback").innerHTML = "That is Correct!!"
 		document.getElementById("hint").innerHTML = "Scroll down to read more"
 		document.getElementById("header").innerHTML = "Info:"
@@ -54,7 +57,7 @@ function caribou(){
 	let input = document.getElementById("ans").value;
 	if(input==="Caribou"|| input==="caribou"){
 		var colorGreen = document.getElementById("feedback");
-		colorGreen.style.color="lawngreen";
+		colorGreen.style.color="limegreen";
 		document.getElementById("feedback").innerHTML = "That is Correct!!"
 		document.getElementById("hint").innerHTML = "Scroll down to read more"
 		document.getElementById("header").innerHTML = "Info:"
@@ -73,11 +76,30 @@ function bobcat(){
 	let input = document.getElementById("ans").value;
 	if(input==="Bobcat"|| input==="bobcat"){
 		var colorGreen = document.getElementById("feedback");
-		colorGreen.style.color="lawngreen";
+		colorGreen.style.color="limegreen";
 		document.getElementById("feedback").innerHTML = "That is Correct!!"
 		document.getElementById("hint").innerHTML = "Scroll down to read more"
 		document.getElementById("header").innerHTML = "Info:"
 		document.getElementById("information").innerHTML = "- This animal can be found in the tundra <br/> - Caribou shed their antler once a year."
+		document.getElementById("filler").innerHTML = "&nbsp"
+		document.getElementById("revealImg").style.display="block";
+	} 
+	else{
+		var colorRed=document.getElementById("feedback");
+		colorRed.style.color="#FF0000";
+		document.getElementById("feedback").innerHTML = "Try again"
+	}
+}
+
+function duck(){
+	let input = document.getElementById("ans").value;
+	if(input==="Duck"|| input==="duck"){
+		var colorGreen = document.getElementById("feedback");
+		colorGreen.style.color="limegreen";
+		document.getElementById("feedback").innerHTML = "That is Correct!!"
+		document.getElementById("hint").innerHTML = "Scroll down to read more"
+		document.getElementById("header").innerHTML = "Info:"
+		document.getElementById("information").innerHTML = "- This animal spends most of it's time near water. <br/> - It is possibly to see them in large groups or bythemselves. <br/> - Their main food souce is the vegitation in their habitat."
 		document.getElementById("filler").innerHTML = "&nbsp"
 		document.getElementById("revealImg").style.display="block";
 	} 
